0e03a67e297d790497b8db1941ecc6dde60cec5e
Julia Grimes <julia.s.grimes@gmail.com>
2022-11-02 19:36:40 -0400

    update to RESTful CRUD syntax

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,15 +1,10 @@
 class ProductsController < ApplicationController
-  def show_products
-    product = Product.first
-    render json: product.as_json
-  end
-
-  def show_all
+  def index
     products = Product.all
     render json: products.as_json
   end
 
-  def one_product
+  def show
     product = Product.find_by(id: params["id"])
     render json: product.as_json
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "/products" => "products#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  get "/products", controller: "products", action: "show_products"
-
-  get "/all_products", controller: "products", action: "show_all"
-
-  get "/one_product/:id" => "products#one_product"
+  get "/products/:id" => "products#show"
 end
