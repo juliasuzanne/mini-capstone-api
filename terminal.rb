a61eab77bf8fdce034c04d3f783a703689b07990
Julia Grimes <julia.s.grimes@gmail.com>
2022-11-01 21:01:27 -0400

    Add to terminal interface ruby file table tty use

--- terminal.rb
+++ terminal.rb
@@ -5,10 +5,21 @@ response = HTTP.get("http://localhost:3000/all_products")
 
 first = HTTP.get("http://localhost:3000/products")
 
+response = response.parse(:json)
 first = first.parse(:json)
 
-$name = first["name"]
+name = first["name"]
+price = first["price"]
+image = first["image_url"]
+des = first["description"]
 
-table = TTY::Table[[$name]["b"]]
+name2 = response[1]["name"]
+price2 = response[1]["price"]
+image2 = response[1]["image_url"]
+des2 = response[1]["description"]
+
+table = TTY::Table[["Name", "Price", "Image", "Description"], [name, "$ #{price}", image, des], [name2, "$ #{price2}", image2, des2]]
+
+puts table.render(:ascii)
 
 # puts table
