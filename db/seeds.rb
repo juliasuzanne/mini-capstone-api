b1be959ce8851da4819b10cf6c351c2ce77b564e
Julia Grimes <julia.s.grimes@gmail.com>
2022-11-02 22:27:37 -0400

    update file with CORS configured

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 Product.create!([
-  {name: "Dark painting", price: 120, image_url: "google.com", description: "A dark painting, it will not haunt you."},
-  {name: "sad_mug", price: 50, image_url: "google.com", description: "This mug is so sad, you can cry into it."},
-  {name: "sad_mug", price: 50, image_url: "google.com", description: "This mug is so sad, you can cry into it."},
-  {name: "Something shiny", price: 1000, image_url: nil, description: "It's too shiny to see what it is."},
-  {name: "A box", price: 0, image_url: "Image", description: "It could be big enough to store your ego."}
+  {name: "Something shiny", price: 0, image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk4fyvmXGE-NP3xW7aYUuFwG4JNArRS5B5cViiBfxMqpN4ZDm68UbmXbMtOUpCpEEAF-I&usqp=CAU", description: "It's too shiny to see what it is."},
+  {name: "sad_mug", price: 0, image_url: "https://earthpigpottery.files.wordpress.com/2019/02/collapsed-pot.jpg", description: "This mug is so sad, you can cry into it."},
+  {name: "A box", price: 0, image_url: "https://m.media-amazon.com/images/I/51-9L697kGL._AC_SY780_.jpg", description: "It's big."},
+  {name: "Dark painting", price: 120, image_url: "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/6a8e4a30696993.56b04596625ce.jpg", description: "A dark painting, it will not haunt you."}
 ])
