55eb64d2a6a05e770c62dcfd15ae34e07c6f1b6b
Julia Grimes <julia.s.grimes@gmail.com>
2022-11-02 20:16:34 -0400

    add seed_dump gem and add post request data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
-product = Product.create(name: "sad_mug", price: 50, image_url: "google.com", description: "This mug is so sad, you can cry into it.")
+Product.create!([
+  {name: "Dark painting", price: 120, image_url: "google.com", description: "A dark painting, it will not haunt you."},
+  {name: "sad_mug", price: 50, image_url: "google.com", description: "This mug is so sad, you can cry into it."},
+  {name: "sad_mug", price: 50, image_url: "google.com", description: "This mug is so sad, you can cry into it."},
+  {name: "Something shiny", price: 1000, image_url: nil, description: "It's too shiny to see what it is."},
+  {name: "A box", price: 0, image_url: "Image", description: "It could be big enough to store your ego."}
+])
