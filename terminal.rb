c0155b9b2bfceb6f27196b8958e0cc44a22db886
Julia Grimes <julia.s.grimes@gmail.com>
2022-11-02 21:06:19 -0400

    update terminal with tty prompt, and add patch functionality to http file.

--- terminal.rb
+++ terminal.rb
@@ -4,24 +4,18 @@ require "tty-prompt"
 
 prompt = TTY::Prompt.new
 
-page = prompt.select("Which page would you like to visit?", %w(products products/:id))
+page = prompt.select("Which page would you like to visit?", %w(products/1 products/2 products/3 products/4))
 
 response = HTTP.get("http://localhost:3000/#{page}")
 
 response = response.parse(:json)
-first = first.parse(:json)
 
-name = first["name"]
-price = first["price"]
-image = first["image_url"]
-des = first["description"]
+name = response["name"]
+price = response["price"]
+image = response["image_url"]
+des = response["description"]
 
-name2 = response[1]["name"]
-price2 = response[1]["price"]
-image2 = response[1]["image_url"]
-des2 = response[1]["description"]
-
-table = TTY::Table[["Name", "Price", "Image", "Description"], [name, "$ #{price}", image, des], [name2, "$ #{price2}", image2, des2]]
+table = TTY::Table[["Name", "Price", "Image", "Description"], [name, "$ #{price}", image, des]]
 
 puts table.render(:ascii)
 
