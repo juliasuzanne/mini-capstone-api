79b7ab16ab7ed8506fd35acf6ab1f37e5dfd185f
Julia Grimes <julia.s.grimes@gmail.com>
2022-11-03 20:00:20 -0400

    adding methods to change friendlier returned data

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,20 @@
 class Product < ApplicationRecord
+  def friendly_created_at
+    created_at.strftime("%B %e, %Y")
+  end
+
+  def is_discounted?
+    discount = false
+    discount = true if price < 10
+  end
+
+  def tax
+    tax = 0
+    tax = price * 0.09
+    tax.round(2)
+  end
+
+  def total
+    total = price + tax
+  end
 end
