d5cc33588ec81f88bc892bcd11479f7253f4270d
Julia Grimes <julia.s.grimes@gmail.com>
2022-11-03 21:11:51 -0400

    Created template for index action on products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   def index
-    products = Product.all
-    render json: products.as_json
+    @products = Product.all
+    render template: "products/index"
   end
 
   def show
