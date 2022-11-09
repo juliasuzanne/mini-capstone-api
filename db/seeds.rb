edf6afa805d2be9b6602d017cb841d48c0ae7ca9
Julia Grimes <julia.s.grimes@gmail.com>
2022-11-08 21:03:52 -0500

    Add supplier id to seeds file for products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 Product.create!([
-  { name: "Something shiny", price: 10, image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk4fyvmXGE-NP3xW7aYUuFwG4JNArRS5B5cViiBfxMqpN4ZDm68UbmXbMtOUpCpEEAF-I&usqp=CAU", description: "It's too shiny to see what it is." },
-  { name: "sad_mug", price: 100, image_url: "https://earthpigpottery.files.wordpress.com/2019/02/collapsed-pot.jpg", description: "This mug is so sad, you can cry into it." },
-  { name: "A box", price: 100, image_url: "https://m.media-amazon.com/images/I/51-9L697kGL._AC_SY780_.jpg", description: "It's big." },
-  { name: "Dark painting", price: 120, image_url: "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/6a8e4a30696993.56b04596625ce.jpg", description: "A dark painting, it will not haunt you." },
+  { supplier_id: 1, name: "Something shiny", price: 10, image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk4fyvmXGE-NP3xW7aYUuFwG4JNArRS5B5cViiBfxMqpN4ZDm68UbmXbMtOUpCpEEAF-I&usqp=CAU", description: "It's too shiny to see what it is." },
+  { supplier_id: 1, name: "sad_mug", price: 100, image_url: "https://earthpigpottery.files.wordpress.com/2019/02/collapsed-pot.jpg", description: "This mug is so sad, you can cry into it." },
+  { supplier_id: 1, name: "A box", price: 100, image_url: "https://m.media-amazon.com/images/I/51-9L697kGL._AC_SY780_.jpg", description: "It's big." },
+  { supplier_id: 1, name: "Dark painting", price: 120, image_url: "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/6a8e4a30696993.56b04596625ce.jpg", description: "A dark painting, it will not haunt you." },
 ])
 
 Supplier.create!([
