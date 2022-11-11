2388f78ae2e2fafa18e39652fa44734225f9180e
Julia Grimes <julia.s.grimes@gmail.com>
2022-11-10 20:41:26 -0500

    Add comments to sessions_controller so I remember what it does

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,10 @@
 class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
+    ## check if username (email) matches encrypted password from digest
+    ## if it does, do the code in the first if statement
     if user && user.authenticate(params[:password])
+      # create jw token
       jwt = JWT.encode(
         {
           user_id: user.id, # the data to encode
@@ -10,6 +13,7 @@ class SessionsController < ApplicationController
         Rails.application.credentials.fetch(:secret_key_base), # the secret key
         "HS256" # the encryption algorithm
       )
+      # below code makes it so that only this app can use this token, access login info (?)
       render json: { jwt: jwt, email: user.email, user_id: user.id }, status: :created
     else
       render json: {}, status: :unauthorized
