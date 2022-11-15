01de3f4153057b879750a4b3ef15fcc42bc49d44
Julia Grimes <julia.s.grimes@gmail.com>
2022-11-14 20:12:14 -0500

    Add comments to describe through relationships with join table

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,9 +2,9 @@ class Category < ApplicationRecord
   has_many :category_products
   has_many :categories, through: :category_products
 
-  def products
-    category_products.map do |category_product|
-      category_product.product
-    end
-  end
+  # def products REPLACED BY LINE 3
+  #   category_products.map do |category_product|
+  #     category_product.product
+  #   end
+  # end
 end
