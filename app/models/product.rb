02ce30cb9b4f89777f79d129f86bad7c79fddf86
Julia Grimes <julia.s.grimes@gmail.com>
2022-11-14 20:09:44 -0500

    Adds join table, categories and products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
   has_many :images
   has_many :orders
   belongs_to :supplier
+  has_many :category_products #order matters here, category_products below
+  has_many :categories, through: :category_products #replaces categories method which calls all the categories for each product called
   validates :name, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :description, length: { in: 10..500 }
@@ -13,6 +15,12 @@ class Product < ApplicationRecord
   def is_discounted?
     price < 10
   end
+
+  # def categories # can only call category_products because of the has_many at top
+  #   category_products.map do |category_product|
+  #     category_product.category
+  #   end
+  # end
 
   def friendly_updated_at
     updated_at.strftime("%B %e, %Y")
