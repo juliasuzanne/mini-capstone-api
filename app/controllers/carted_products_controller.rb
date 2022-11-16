2cdcab7c51a337d13512ccbcdc49f35942fbcae7
Julia Grimes <julia.s.grimes@gmail.com>
2022-11-15 22:34:17 -0500

    Working on CRUD functionality within carted_products methods

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -4,12 +4,18 @@ class CartedProductsController < ApplicationController
       user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
-      status: params[:status],
+      order_id: nil,
+      status: "carted",
     )
     if cart.save
       render json: cart.as_json
     else
       render json: { errors: cart.errors.full_messages }, status: :unprocessable_entity
     end
+  end
+
+  def index
+    carted = current_user.carted_products
+    render json: carted.as_json
   end
 end
